8b6e63105fbb9ce07862eb1a37278cfdabafb358
TRAN Quoc Dam <qtran3@amaris.com>
2020-08-24 18:07:40 +0700

    add list api

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./model/User');
 var {Todo} = require('./model/todo');
+const { response } = require('express');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/Todos', (req, res) => {
+app.post('/todos', (req, res) => {
     //console.log(req.body)
     var todo = new Todo({
         text: req.body.text
@@ -22,47 +23,19 @@ app.post('/Todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    }). catch((e) => console.log(e));
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
 module.exports = {app};
-// // save new something
-// var Todo = mongoose.model('Todo', {
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt: {
-//         type: Number,
-//         default: null
-//     }
-// });
-
-// var otherTodo = new Todo({
-//     text: 'haha'
-// });
-
-// otherTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('error', e);
-// });
-
-
-// var user = new User({
-//     email: 'qtran3@gmail.com'
-// });
-
-// user.save().then((doc) => {
-//     console.log('User saved', doc);
-// }, (e) => {
-//     console.log('Unable to save user', e);
-// });
