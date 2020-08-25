2864f7f65774879d4a77c1e98c138ad4ce619aae
TRAN Quoc Dam <qtran3@amaris.com>
2020-08-25 12:52:12 +0700

    add delete

--- server/server.js
+++ server/server.js
@@ -50,7 +50,22 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
 
+app.delete('/todos/:id', (req, res) => {
+    // Get the id
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    // remove todo by Id
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(400).send("can not find the ID");
+        };
+        return res.send({todo});
+    }).catch((e) => res.status(400).send(e));
 });
 
 app.listen(port, () => {
