84dad75a6707bacd751a0dc667ff321d4fa700c3
TRAN Quoc Dam <qtran3@amaris.com>
2020-08-25 09:50:02 +0700

    all

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//var test = 'mongodb+srv://admin:@Bc1234567890@cluster0.qzna1.mongodb.net/Todos?retryWrites=true&w=majority'
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect(test, { useNewUrlParser: true, useUnifiedTopology: true });
+var test = 'mongodb+srv://admin:@Bc1234567890@cluster0.qzna1.mongodb.net/Todos?retryWrites=true&w=majority'
+//mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(test, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = {
     mongoose
