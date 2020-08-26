befbae165047092d7c2e63e8584021be5502df94
TRAN Quoc Dam <qtran3@amaris.com>
2020-08-26 15:37:21 +0700

    hash pass word

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 const {ObjectID} = require('mongodb')
+const bcrypt = require('bcryptjs')
 
-var data = {
-             id: 4
-        };
-console.log(data.toObject())
-var token = jwt.sign(data, 'secretkey');
-//console.log(token);
+var password = '123456';
 
-var object = new ObjectID('5f44f037d7c4bbdcb8c4a5b7')
-console.log(object.toString())
+bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+                console.log(hash)
+        })
+})
 
-var decoded = jwt.verify(token, 'secretkey');
+var hashedPassword = '$2a$10$2TaUy/PBMFNRqtfkHZrB/.X0LFrgtYWTImOg79SgrthTKjbcv19vS'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+        console.log(res)
+})
+
+
+// var data = {
+//              id: 4
+//         };
+// console.log(data.toObject())
+// var token = jwt.sign(data, 'secretkey');
+// //console.log(token);
+
+// var object = new ObjectID('5f44f037d7c4bbdcb8c4a5b7')
+// console.log(object.toString())
+
+// var decoded = jwt.verify(token, 'secretkey');
 //console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -36,3 +52,5 @@ var decoded = jwt.verify(token, 'secretkey');
 // } else {
 //     console.log('data was changed. huhu')
 // }
+
+
