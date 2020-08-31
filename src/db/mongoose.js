cbf57387f16acf3018e36053a5d009b090a67a4b
TRAN Quoc Dam <qtran3@amaris.com>
2020-08-31 10:15:39 +0700

    a

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //var test = 'mongodb+srv://admin:@Bc1234567890@cluster0.qzna1.mongodb.net/Todos?retryWrites=true&w=majority'
-
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //mongoose.set('useCreateIndex', true);
 //mongoose.connect(test, { useNewUrlParser: true, useUnifiedTopology: true });
